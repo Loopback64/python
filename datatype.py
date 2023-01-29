e0dc92f93ca7e60893d9bb001bb5d837ba2e1082
loopback64 <50466857+Loopback64@users.noreply.github.com>
2023-01-29 02:59:58 -0500

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,34 @@
+
+# Strings
+
+print("Hello world")
+print("Hello World")
+print('Hello World')
+print('''Hello World''')
+print(""" Hello World""")
+
+print(type("Hello world"))
+
+print("Bye" + "World")
+
+
+# Integer
+print(30)
+
+
+# Float
+print(30.5)
+
+# Boolean
+type(False)
+type(True)
+
+True
+False
+ññl{}
+
+# List
+
+ [10, 20, 30, 55] 
+ ['Hello', 'Bye', 'Adios']
+ [10, "Hello", True, 10]
---
+++ helloworld.py
@@ -0,0 +1,9 @@
+#Este es un comentario no sera interpretado por el código
+#Este codigo imprime un mensaje por pantalla
+print("Hello World")
+
+
+
+
+
+
