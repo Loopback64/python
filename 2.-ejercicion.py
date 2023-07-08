840db7b8e9f52a840e80c308626d11c09501b420
loopback64 <loopback64@criptext.com>
2023-07-08 01:35:04 -0500

    Calcula el promedio de notas

---
+++ 2.-ejercicion.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#MensajeInicial: Nombre del Algoritmo 
+print("-------------------------------------------------------")
+print("Ejercicio2: PROMEDIO DE 3 NOTAS.")
+print("-------------------------------------------------------")
+
+#Entradas
+
+print("Ingrese las 3 notas del alumno N1 , N2, N3")
+
+N1 = int( input("N1: "))
+N2 = int( input("N2: "))
+N3 = int( input("N3: "))
+
+#Proceso
+P = int( (N1+N2+N3)/3 )
+
+#Salida
+
+print("\nSalida: ")
+print(P)
+
+
+
+
+
