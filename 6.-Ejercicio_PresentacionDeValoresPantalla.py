9cb22ede0f2ff0473e20c9aaf8334452379a6c41
loopback64 <luishever@gmail.com>
2023-07-29 02:16:00 -0500

    6.-Ejercicio_PresentacionDeValoresPantalla.py

---
+++ 6.-Ejercicio_PresentacionDeValoresPantalla.py
@@ -0,0 +1,31 @@
+#Presentación de valores en pantalla
+
+"""Muestre en pantalla un conjunto de tipos de datos: string, entero, lógico y un arreglo."""
+
+#Solucion : El problema requiere mostrar un conjunto de datos que se pueden utilizar.
+
+
+nombre = "Mi amigo Sebastian "
+
+numero1 = 25
+
+logico  = False 
+
+lista = [1, 2, 3, 4, 5 ]
+
+print(nombre)
+print(numero1)  
+print(logico   )
+print(lista[0]    )
+print(lista[1]    )
+print(lista[2]     )
+
+
+
+
+
+
+
+
+
+
