202dc5c8157af622c831f5ee9029f0191e2808ec
loopback64 <luishever@gmail.com>
2023-07-29 01:09:54 -0500

    SE dibuja un rectangulo con asteriscos

---
+++ 5.-Ejercicio_PresentacionDeCaractereesEnPantalla.py
@@ -0,0 +1,22 @@
+# Muestre en pantalla un rectángulo de asteriscos.
+
+
+""" Solucion: El problema requiere mostrar un rectángulo hecho con asterisco *. Utilizaremos varias líneas
+con la función print( )."""
+
+
+print("*******************************")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*                             *")
+print("*******************************")
