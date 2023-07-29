a910fd578fa04e79858d1145f320cd682eaf04d4
loopback64 <luishever@gmail.com>
2023-07-28 23:31:48 -0500

    Subiendo problemas resueltos

---
+++ 1.-Ejercicio_Conversion.py
@@ -0,0 +1,33 @@
+#Conversión de grados de Temperatura
+
+"""Realice un algoritmo que realice la conversión de grados Fahrenheit a Celsius. Utilice la
+fórmula adecuada"""
+
+
+F = input(" Ingrese un valor =  "   ) 
+C = round(( int(F)-32)/1.8   )
+
+print("C: ", C    )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
