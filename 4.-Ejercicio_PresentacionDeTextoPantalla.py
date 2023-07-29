ac410522572cca6177082bea8fdac5dfe543b235
loopback64 <luishever@gmail.com>
2023-07-29 01:01:45 -0500

    Se muestra la informacion el pa pantalla, usando saltod de linea o continuando en la misma linea, agregando espacios o quitando espacios al imprimir

---
+++ 4.-Ejercicio_PresentacionDeTextoPantalla.py
@@ -0,0 +1,38 @@
+#Muestre en pantalla su nombre y apellido.
+"""El problema requiere mostrar su nombre y apellido. Utilizaremos la función print."""
+
+print('loopback64 loopback64')
+
+
+print("---------------------------------------")
+
+print("\tloopback64\tloopback64")
+
+
+print("---------------------------------------")
+
+print("\tloopback64\tloopback64",end="")
+
+
+print("---------------------------------------")
+
+
+print("\nloopback64","loopback64",sep="",end="")
+
+
+print("---------------------------------------")
+
+print("\nloopback64","loopback64",end="")
+
+
+
+print("---------------------------------------")
+
+
+
+print("\nloopback64","loopback64",sep="")
+
+
+
+
+print("---------------------------------------")
