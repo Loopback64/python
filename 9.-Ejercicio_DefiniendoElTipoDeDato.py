88472558600abde079b531f30a7ac61ac1525aa8
loopback64 <luishever@gmail.com>
2023-07-30 01:04:06 -0500

    Definiendo tipos de datos

---
+++ 9.-Ejercicio_DefiniendoElTipoDeDato.py
@@ -0,0 +1,25 @@
+# Definiendo el tipo de dato
+
+# Definir un número de tipo entero y uno de tipo real, luego asigne un valor coherente y finalmente presente su contenido en pantalla.
+
+"""Solución: (Análisis)
+El problema a simple vista requiere declarar dos tipos de datos uno entero y otro real, asigne el
+nombre que desee, siempre que no sea una palabra reservada. Luego asigne un número entero
+y otro real a cada variable creada. Presente en pantalla como en los anteriores programas."""
+
+
+numero = int()
+numero = 4
+sueldo = float()
+sueldo = 2600
+
+print(numero," ", sueldo)
+
+
+"""
+Línea 11: Declaración int.
+Línea 12: Asignación.
+Línea 13: Declaración float.
+Línea 14: Asignación.
+Línea 16: Presentación."""
+
