69f5ef021e71a552e85e1c60d9954a85b69b32b3
loopback64 <luishever@gmail.com>
2023-07-31 23:53:18 -0500

    Determine si una persona es mayor de edad

---
+++ 17.-DeterminaValorResultanteVariables.py
@@ -0,0 +1,30 @@
+# Determine el valor resultante de las variables solicitadas
+# Escriba un programa que permita encontrar el valor final de las variables solicitadas.
+
+"""Si se observa el conjunto de columnas que van en orden, debemos ir realizando un
+seguimiento, línea a línea, de arriba abajo de cada una de las variables [21].
+
+
+
+
+
+
+Ejemplo, si la primera línea a es igual a 1 y la segunda b es igual a 2, entonces la tercera línea
+que es c, tiene el valor de a sumado b."""
+
+
+
+a = 1
+b = 2
+c = a + b 
+a = c 
+c = b
+a = c - b
+b = a + 1
+c = b + 4
+c = b
+
+print("a:  ",a)
+print("b:  ",b)
+print("b:  ",c)
+
---
+++ 18.-DeterminePersonaMayorEdad.py
@@ -0,0 +1,18 @@
+#Determine si una persona es mayor de edad
+"""Escriba un programa que permita el ingreso de un número entero, que simbolice la edad de
+una persona, muestre a continuación sólo si es mayor de edad.
+
+"""
+n = int(input("Ingrese su edad:  "))
+if (n>=18): 
+    print("Es mayor de edad")
+
+else: 
+    print("Es menor de edad")
+
+
+    
+
+
+
+
