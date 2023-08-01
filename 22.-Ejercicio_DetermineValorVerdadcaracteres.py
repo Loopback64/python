38e87b6b8decfdcf2b017e5c53b56166bb2c8d4c
loopback64 <luishever@gmail.com>
2023-08-01 00:32:51 -0500

    Determine el valor de verdad entre dos cadenas de caracteres

---
+++ 22.-Ejercicio_DetermineValorVerdadcaracteres.py
@@ -0,0 +1,21 @@
+# Determine el valor de verdad entre dos cadenas de caracteres
+"""Escriba un programa que permita comparar dos cadenas de caracteres usando if.
+ Solución: (Análisis)
+ Más adelante trataremos cadenas de caracteres y formas de evaluarlas. Por ahora usaremos la
+ estructura if en el mejor de los casos. Sólo se da a escoger por verdadero. """
+
+palabra = "gato"
+
+if (palabra == "gato"):
+    print(palabra == "gato")
+if (palabra != "perro"):
+    print(palabra != "gato")
+if (palabra >= "gato"):
+    print(palabra >= "gato")
+if (palabra <= "gato"):
+    print(palabra <= "gato")
+
+
+
+
+
