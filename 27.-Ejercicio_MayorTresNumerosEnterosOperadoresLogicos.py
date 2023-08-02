16a6ee3f0605401b246561f2f165f274f7e10b3c
giwavow630 <giwavow630@sparkroi.com>
2023-08-02 16:59:11 -0500

    El mayor de tres números enteros con operadores lógicos

---
+++ 27.-Ejercicio_MayorTresNumerosEnterosOperadoresLogicos.py
@@ -0,0 +1,31 @@
+# El mayor de tres números enteros con operadores lógicos
+
+""" Escriba un programa que permita visualizar cuál es el mayor de tres números enteros ingresados
+por teclado, utilice operadores lógicos.
+
+Solución: (Análisis)
+Dado el diagrama de flujo para su comprensión, hay que recordar que la validación entre
+números depende de las condiciones que agreguemos para ir filtrando cada posibilidad de
+comparación. A medida que se desee validar más números, más bifurcaciones.  """
+
+
+a = int(input("Ingrese el valor de a: "))
+b = int(input("Ingrese el valor de b: "))
+c = int(input("Ingrese el valor de c: "))
+
+if( (a>b) and (a>c) ):
+    print(a, " es mayor ")
+else:
+    if( (b>c) and (b>c) ):
+        print(b, "es mayor")
+    else:
+        if( (c>a) and (c>b) ):
+            print(c, "es mayor")
+        else:
+            print("son iguales")
+
+        
+
+
+
+    
