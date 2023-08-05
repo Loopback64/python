55cbbf54854863f3b8f6b35fb6f31d6f4f188687
loopback64 <luishever@gmail.com>
2023-08-05 13:19:39 -0500

    Clases objetos

---
+++ 35.-Ejercicio_ValidenumeroDentroUnRango.py
@@ -0,0 +1,21 @@
+# Valide un número dentro de un rango.
+""" Escriba un programa que, en un determinado rango, se valide un número. Utilice varias formas
+de escribir el mismo programa usando los operadores de relación y operadores lógicos.
+
+Solución: (Análisis)
+Nos piden validar si un número es menor a cero (sin incluir), que esté entre 0 (inclusive) a 5
+(inclusive) y si es mayor a 5 (sin incluir).
+
+"""
+
+
+
+num1 = 4
+
+if(num1>=0 and num1<=5):
+    print("Buen ingreso")
+else:
+    print("Mal ingreso")
+
+
+
---
+++ Clases y Objetos.py
@@ -0,0 +1,22 @@
+Celular1_marca = "Samsung"
+Celular2_marca = "apple"
+Celular3_marca = "huawei"
+
+Celular1_modelo = "S23"
+Celular2_modelo = "iphone 15 pro"
+Celular3_modelo = "P20 pro"
+
+Celular1_camaraT = "48MP"
+Celular2_camaraT= "48MP"
+Celular3_camaraT = "12MP"
+
+Celular1_camaraF = "24MP"
+Celular2_camaraF = "24MP"
+Celular3_camaraF = "8MP"
+
+class Celular():
+    marca = "Samsung"
+    modelo = "S23"
+    camara = "48"
+
+
