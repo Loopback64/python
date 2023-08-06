8d380b6452460db0cda5b0fc8982904ccfa0008f
loopback64 <luishever@gmail.com>
2023-08-06 07:35:37 -0500

    Valide un número dentro de un rango de valores usando and, or

--- 35.-Ejercicio_ValidenumeroDentroUnRango.py
+++ 35.-Ejercicio_ValidenumeroDentroUnRango.py
@@ -10,12 +10,79 @@ Nos piden validar si un número es menor a cero (sin incluir), que esté entre 0
 
 
 
-num1 = 4
+num1 = int(input("Ingrese el número: "))
 
 if(num1>=0 and num1<=5):
     print("Buen ingreso")
 else:
     print("Mal ingreso")
+
+if(num1<0 or num1>5):
+    print("Mal ingreso")
+else:
+    print("Buen ingreso")
+
+### Bloque 2
+
+if(num1>-1 and num1<6):
+    print("Buen ingreso")
+else:
+    print("Mal ingreso")
+
+if(num1<=-1 or num1>=6):
+    print("Mal ingreso")
+else:
+    print("Buen ingreso")
+
+
+### Bloque 3
+
+
+if not(num1>-1 and num1<6):
+    print("Mal ingreso")
+else:
+    print("Buen ingreso")
+
+if not(num1<0 or num1>5):
+    print("Buen ingreso")
+else:
+    print("Mal ingreso")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
