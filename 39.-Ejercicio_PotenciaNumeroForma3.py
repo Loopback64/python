29b55e42e9b61da0a66ba6b566cd15885b593ac0
loopback64 <luishever@gmail.com>
2023-08-07 22:48:57 -0500

    La potencia de un número.

---
+++ 39.-Ejercicio_PotenciaNumeroForma3.py
@@ -0,0 +1,20 @@
+# La potencia de un número.
+
+""" Escriba un programa que permita el ingreso de dos números enteros, uno representa la base y
+el otro el exponente de la operación matemática conocida como potenciación.
+
+"""
+
+import math as mat 
+base = int(input("Base: "))
+expo = int(input("Exponente: "))
+acum = mat.pow(base,expo)
+print(acum)
+
+
+
+
+
+
+
+
