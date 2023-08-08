14397a4f3814835acc396dd46bb6a9ecf7628559
loopback64 <luishever@gmail.com>
2023-08-07 23:30:43 -0500

    Hallar cos (x) usando la serie de Taylor

---
+++ 41.-Ejercicio_Hallar_cos{x}_usando_serie_de_taylor.py
@@ -0,0 +1,58 @@
+# Hallar cos (x) usando la serie de Taylor
+
+""" Escriba un programa que permita obtener el resultado de la serie de Taylor, cos(x).
+
+Solución: (Análisis)
+El problema es similar al anterior, pero ahora debemos considerar la alternancia de los signos y
+los números de las potencias y factorial son pares, se puede observar que cuando nos
+encontramos en la iteración i, de valor impar, los términos deben restar y cuando estamos en la
+iteración i, de valor par, los términos deben sumarse, al acumulador de sumas (términos).
+
+"""
+
+x = int(input("Ingrese x: "))  # x = 1
+
+n = int(input("Ingrese n: "))  # n = 3
+
+acum = 0
+
+for i in range(1, (n), 1):
+    ii = i*2
+    # potencia
+    pot = 1
+    for  j in range (1, (ii + 1), 1):
+        pot = pot * x 
+    # factorial
+    fac = 1  
+    for j in range(1, (ii + 1), 1):
+        fac = fac * j
+
+    if(i % 2 == 1):
+        acum = acum - (pot/fac)
+
+    else:
+        acum = acum + (pot/fac)
+print("e^x = ",(acum + 1))
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
