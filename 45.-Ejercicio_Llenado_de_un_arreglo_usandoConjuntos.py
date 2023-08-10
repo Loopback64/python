8ac465987041546c6968e85b1a12cb5c866b40af
loopback64 <luishever@gmail.com>
2023-08-09 23:13:37 -0500

    Usando conjuntos Se define un conjunto de elementos, correspondientes a los días de la semana.

---
+++ 45.-Ejercicio_Llenado_de_un_arreglo_usandoConjuntos.py
@@ -0,0 +1,28 @@
+# Se define un conjunto de elementos, correspondientes a los días de la semana. usando conjuntos
+
+
+
+conjunto1 = { "Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado","Domingo",}
+
+conjunto2 = { "Martes" , "Jueves" }
+
+print(conjunto1)
+print(conjunto2)
+
+for i in conjunto1:
+    print(i, end=", ")
+
+print()
+
+print("Interseccion: ", conjunto1 & conjunto2)
+
+
+
+
+
+
+
+
+
+
+
