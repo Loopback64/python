e8c92143f49028ce61164e3d49bbe995eadae426
loopback64 <luishever@gmail.com>
2023-08-20 18:20:34 -0500

    Funcion suma de dos numeros

---
+++ 54.-Ejercicio_Funcion_metodo_retorno.py
@@ -0,0 +1,61 @@
+
+"""
+
+Anteriormente se realizó la suma de dos números, se requiere dos variables de entrada, n1 y n2,
+luego una operación como suma = n1 + n2 y finalmente se muestra la variable de salida suma.
+
+
+"""
+
+def suma(num1 , num2):
+    sumato = num1 + num2
+    return sumato
+
+y = suma(5,4)
+
+print( "La suma de 5+4 es :  " , y )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
