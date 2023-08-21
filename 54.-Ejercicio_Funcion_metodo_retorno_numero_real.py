720a807643f3a2fe292e030a10374fe5396d5d2e
loopback64 <luishever@gmail.com>
2023-08-20 21:37:56 -0500

    Ejercicion metodo reotrno numero real

---
+++ 54.-Ejercicio_Funcion_metodo_retorno_numero_real.py
@@ -0,0 +1,32 @@
+"""
+Anteriormente se realizó la suma de dos números con una función, ahora es la división, se debe
+observar que la operación requiere de un número real, es decir la variable que recibe el valor
+devuelto por la función debe ser del mismo tipo de dato de la variable retornada dentro de la
+función.
+
+"""
+
+
+
+def division(n1,n2 = 3 ):
+    divi = n1 / n2 
+    return divi 
+
+resultado = division(27)
+print("La division es:  " , resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
