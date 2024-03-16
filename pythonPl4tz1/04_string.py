e99318946fed0a3113916eb499ec9c0fdc8e06c3
loopback64 <luishever@gmail.com>
2024-03-15 23:28:38 -0500

    Usando strings en el codigo

---
+++ pythonPl4tz1/04_string.py
@@ -0,0 +1,10 @@
+name = "Nicolas"
+last_name = "Molina Monroy"
+# Imprime los datos de las variables
+print(name)
+print(last_name)
+
+full_name = name + last_name
+print(full_name)
+
+quote = 'I am Nicolas'
