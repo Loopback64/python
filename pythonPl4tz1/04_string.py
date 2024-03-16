1a605dfc223a8dc8fbe8ad5cdbd383bad5777e39
loopback64 <luishever@gmail.com>
2024-03-15 23:34:45 -0500

    Usando strings en el codigo

--- pythonPl4tz1/04_string.py
+++ pythonPl4tz1/04_string.py
@@ -8,3 +8,14 @@ full_name = name + last_name
 print(full_name)
 
 quote = 'I am Nicolas'
+print(quote)
+
+quote2=' She said "Hello"  '
+print(quote2)
+
+# format
+template = "Hola, mi nombre es " + name +   "  y mi apellido es " + last_name
+print( 'V1' , template )
+
+template = "Hola, mi nombre es {} y mi apellido es {}" .format()
+
