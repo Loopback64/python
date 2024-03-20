6fec83c90428c227a965331267f0027c6bf4fe17
loopback64 <luishever@gmail.com>
2024-03-20 00:58:14 -0500

    Usamos transformación de variables , strings , enteros.

---
+++ pythonPl4tz1/07.-transformation.py
@@ -0,0 +1,22 @@
+name = "Nicolas"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Nicolas " + "Molina")
+print(10 + 20)
+print("Nicolas" + "20")
+
+age = 12
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input('Escribe tu edad => ')
+print(type(age))
+age = int(age)
+print(f'Tu edad en 10 años sera {age}')
+
+
+
