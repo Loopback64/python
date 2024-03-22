adfb09a080aa3aa0e10b4e6581f5f3821f1640e0
loopback64 <luishever@gmail.com>
2024-03-22 00:41:08 -0500

    Usamos operadores de comparación, comparamos números y strings.

---
+++ pythonPl4tz1/09.-comparators.py
@@ -0,0 +1,47 @@
+# > mayor que
+print(7 > 3) # Verdad
+print(3 > 7) # False
+print(7 > 7) # False
+print("-----------------")
+
+# < menor que
+
+print(5 < 6) # verdad True
+print(6 < 5) # Falso False
+print(5 < 5) # Falso False
+print("-----------------")
+
+# <= menor o igual
+
+print(1 <= 2) # Verdad True
+print(2 <= 1) # Falso False
+print(2 <= 2) # Verdad True
+print("-----------------")
+
+# == igual
+
+print(6 == 6)
+print(5 == 2)
+print("-----------------")
+
+# !=
+print("-----------------")
+print(6 != 10)
+print(6 != 6)
+print("-----------------")
+print("Applee" == 'Apple')
+print("Applee" == 'apple')
+print("-----------------")
+
+age = 18
+print(age >= 15)
+
+
+
+
+
+
+
+
+
+
--- pythonPl4tz1/9.-Operadores.py
+++ pythonPl4tz1/9.-Operadores.py
@@ -1,1 +1,2 @@
-print()
+# Operadores de comparación
+
