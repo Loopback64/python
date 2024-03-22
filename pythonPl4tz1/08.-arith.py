d14388e96e8ae264dabc02a7016aee6c676a1ba9
loopback64 <luishever@gmail.com>
2024-03-22 00:16:45 -0500

    Usamos transformación de variables , strings , enteros.

--- pythonPl4tz1/08.-arith.py
+++ pythonPl4tz1/08.-arith.py
@@ -2,12 +2,15 @@ print(10 + 10)
 print(10 - 5)
 print(10 * 2)
 print(10 / 2)  # 5
-print(10 % 2)  # 0
-print(10 / 3)
-print(10 % 3)
-print(10 // 3)
-print(2 ** 3)
-print(2 ** 3 + 3 - 7 / 1 // 4)
+print(10 % 2)  # 0 Operador módulo
+print(10 / 3)  # 3.33333 Division exacta
+print(10 % 3)  # Residuo 1
+print(10 // 3) # 3 Resultado de la dision redondeando a un entero
+print(2 ** 3)  # 8 Potencia de un número
+print(2 ** 3 + 3 - 7 / 1 // 4)  # 11 - 1 = 10
+print(2 ** 3)  # 8
+print((7 / 1) // 4) #
+# print(10 / 0)  Error !! no se puede dividir
 
 print('Hola' + 'mundo')
 print('Hola' * 3)
