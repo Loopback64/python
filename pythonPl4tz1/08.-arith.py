73338cb996d2270d56b623f3b8968ea94c849451
loopback64 <luishever@gmail.com>
2024-03-21 23:56:10 -0500

    Usamos transformación de variables , strings , enteros.

---
+++ pythonPl4tz1/08.-arith.py
@@ -0,0 +1,19 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2)  # 5
+print(10 % 2)  # 0
+print(10 / 3)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3)
+print(2 ** 3 + 3 - 7 / 1 // 4)
+
+print('Hola' + 'mundo')
+print('Hola' * 3)
+
+
+
+
+
+
