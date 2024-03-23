cdc6e26aa12beca118b5ce1a444e1eb84ac9ab20
loopback64 <luishever@gmail.com>
2024-03-23 01:07:47 -0500

    Operadores lógicos, ahora usamos el OR y AND.

---
+++ pythonPl4tz1/11_logic.py
@@ -0,0 +1,41 @@
+# and
+print('AND')
+print('True and True =>', True and True)
+print('True and False =>', True and False)
+print('False and True =>', False and True)
+print('False and False =>', False and False)
+
+print(10 > 5 and 5 < 10)
+print(10 > 5 and 5 > 10)
+
+stock = input('Ingrese el número de stock => ')
+print(type(stock))
+print("----------")
+stock = int(stock)
+print(type(stock))
+print("----------")
+
+
+print(stock >= 100 and stock <= 1000)
+print("----------")
+
+# Operador OR
+
+print('OR')
+
+print('True or  True =>', True or True)
+print('True or False =>', True or False)
+print('False or True =>', False or True)
+print('False or False =>', False or False)
+
+
+role = input('Digita el role =>')
+
+print(role == 'admin' or role == 'seller')
+
+
+
+
+
+
+
