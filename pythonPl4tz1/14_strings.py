956e8216b9aa16fe17543f202a325a9383e42ffc
loopback64 <luishever@gmail.com>
2024-03-31 23:47:46 -0500

    Metodo para contar variables

---
+++ pythonPl4tz1/14_strings.py
@@ -0,0 +1,45 @@
+text = 'Ella sabe programar en Python'
+
+'''
+print('JavaScript' in text) #Busca si la cadena de text Javascript esta e texto
+print('Python' in text)      #Busca si la cadena de texto Python esta en el texto
+
+if 'Python' in text:
+    print('Elegiste bien !!')
+
+else:
+    print('Tambien elegiste bien')
+'''
+
+
+
+
+size = len(text)
+print(size)
+print("-------------")
+
+print(text)
+print(text.upper())
+print(text.lower())
+
+print("-------------")
+print("Contar el numero de veces que aparece una letra en el texto")
+
+print(text.count('a'))
+
+print("-------------")
+print("Método Swap case")
+
+
+print(text.swapcase())
+print(text.startswith('Ella'))
+
+
+
+
+
+
+
+
+
+
