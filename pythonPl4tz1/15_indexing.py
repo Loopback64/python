035285843c3faed78b1e5e62109828d42901b8e3
loopback64 <luishever@gmail.com>
2024-04-02 23:55:49 -0500

    Strings, sacando variables

--- pythonPl4tz1/15_indexing.py
+++ pythonPl4tz1/15_indexing.py
@@ -1,10 +1,22 @@
 text = "Ella sabe Python"
 print(text[0])
 print(text[1])
+
 # print(text[999])
+
 size = len(text)
 size('size =>', size)
 print(text[size - 1])
-print
+print(text[-1])
+
+# slicing
+
+print(text[0:5])
+print(text[10:16])
+print(text[:10])
+
+
+
+
 
 
