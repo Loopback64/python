26df0fd2b531f7f07b2a423db87e465a44686f0d
loopback64 <luishever@gmail.com>
2024-04-05 01:02:02 -0500

    Introduccion a listas

---
+++ pythonPl4tz1/16_lists.py
@@ -0,0 +1,28 @@
+numbers = [ 1 , 2 , 3 , 4 ]
+print(numbers)
+print(type(numbers))
+
+tasks = ['make a dishes' , 'play videogames']
+print(tasks)
+
+types = [1 , True, 'hola']
+print(types)
+
+print(numbers[0])
+print(tasks[0])
+text = 'Hola'
+# text[0] = 'w'
+
+tasks[0] = 'watch platzi courses'
+print(tasks)
+
+tasks[0] = 'do the dishes'
+print(tasks)
+
+print(numbers[:3])
+print(True in types)   # proposicion si existe un True en la tabla types
+print('hola' in types)
+
+
+
+
