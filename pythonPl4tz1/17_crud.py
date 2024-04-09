09d376ce8af15a9f87d0de2dfc98d1db6d9dbe4a
loopback64 <luishever@gmail.com>
2024-04-09 02:14:47 -0500

    Crea leer actualizar o borrar datos de las listas.

--- pythonPl4tz1/17_crud.py
+++ pythonPl4tz1/17_crud.py
@@ -2,5 +2,17 @@
 
 numbers = [1, 2, 3, 4, 5]
 print(numbers[1])
-khhkjhkjh
+numbers[-1] = 10
+print(numbers)
+
+numbers.append(700) #agrega a la ultima posicion el entero 700
+print(numbers)
+
+numbers.insert(0,'hola')
+print(numbers)
+
+numbers.insert(3,'change')
+print(numbers)
+
+
 
