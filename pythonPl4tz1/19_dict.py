1b355429afadd9816f35199954667dca311c8f1c
loopback64 <luishever@gmail.com>
2024-04-10 00:42:48 -0500

    Crea leer modificar diccionarios.

---
+++ pythonPl4tz1/19_dict.py
@@ -0,0 +1,30 @@
+my_dict = {}    # los diccionarios se escriben entre corchetes
+print(type(my_dict))      #
+
+my_dict = {
+    'avion'     : 'bla bla bla bla ' ,
+    'name'      : "Nicolas"          ,
+    'last_name' : 'Molina Monroy'    ,
+    'age'       : "87"                ,
+
+}
+
+print(my_dict)
+print(len(my_dict))
+
+#puedo hacer consultas al diccionario usando
+print("------------")
+print(my_dict["age"])
+print(my_dict['last_name'])
+print(my_dict.get('unvalor'))
+
+
+
+
+
+
+
+
+
+
+
