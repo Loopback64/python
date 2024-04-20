338fa64138e81bb852225b44f89bc563f002a244
loopback64 <luishever@gmail.com>
2024-04-20 00:39:26 -0500

    Agregamos mas metodos para trabajar conlistas

--- pythonPl4tz1/17_crud.py
+++ pythonPl4tz1/17_crud.py
@@ -56,9 +56,90 @@ print("removemos parte de la lista usando el metodo remove,  todo 1")
 new_list.remove('todo 1')
 print(new_list)
 
+# metodo que elimina automaticamente el ultimo elemento de una lista .pop
+
+
+print('-------------')
+print('-------------')
+
+print('metodo que elimina automaticamente el ultimo elemento de una lista .pop')
+
+new_list.pop()
+print(new_list)
+
+#Tambien puede eleminiar un elemento que ocupa un lugar en la lista, por ejemplo el elemento de de orden 0
+
+print('Elimina un elemento de la lista usando el metodo .pop, con usando el lugar que ocupa en este ejemplo el lugar 0 , es decir el elemento hola ')
+new_list.pop(0)
+print(new_list)
+
+#Usando el metodo reverse
+print('-------------')
+print('-------------')
+
+print('Usamos el metodo .reverse para poder revertir o invertir el orden de la lista')
+
+
+new_list.reverse()
+print(new_list)
+
+
+#Usamos un metodo para ordenar una lista de menor a mayor
+
+print('-------------')
+print('-------------')
+
+print('Usamos el metodo .sort para ordenar una lista de numeros de menor a mayor ')
+
+numbers_a = [  1, 4, 6 ,3 ]
+print(numbers_a)
+numbers_a.sort()
+
+print(numbers_a)
+
+
+
+print('-------------')
+print('-------------')
+
+#Ahora ordenamos strings en la lista re , ab , ed   usando el metodo sort
+
+strings = [ 're' , 'ab' , 'ed'  ]
+
+print(strings)
+
+strings.sort()
+
+print(strings)
+
+#El metodo sort no funciona cuando tenemos datos mezclados como numero y letras 
+print('El metodo .sort no funciona cuando tenemos numeros con letras mezclados')
+print('-------------')
+print('-------------')
+
+print('Usando lista de numeros y letras para probar metodo .sort')
+#sale un error
+#   print(new_list)
+#   new_list.sort()
 
 
 
 
 
 
+print('-------------')
+print('-------------')
+
+
+print('-------------')
+print('-------------')
+
+
+
+print('-------------')
+print('-------------')
+
+
+
+print('-------------')
+print('-------------')
