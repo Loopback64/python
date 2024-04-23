6c573ddb08dd4ebbbf11dca6170553f2f6809d29
loopback64 <luishever@gmail.com>
2024-04-22 23:58:29 -0500

    Crea leer modificar diccionarios.

--- pythonPl4tz1/20_dict.py
+++ pythonPl4tz1/20_dict.py
@@ -1,9 +1,37 @@
+print("******************************************************************")
+print("******************************************************************")
+
+print("Mostramos los valores cargados en el diccionario llamado person")
+print("-----------------------------------------------------------------")
+
 person = {
     'name' : 'nico',
     'last_name' : 'molina',
     'langs' : ['python' , 'javascript'],
-
-
-
-
+    'age' : 99
 }
+
+print(person)
+
+print("-----------------------------------------------------------------")
+print("-----------------------------------------------------------------")
+print("-----------------------------------------------------------------")
+print("Ahora cambiamos el valor name a santi --- a la edad le restamos 50 , agregamos rust al final de la lista")
+print("y mostramos el resultado")
+print("-----------------------------------------------------------------")
+
+
+
+person['name'] = 'santi'
+person['age'] -= 50 # al valor actual le restamos 50
+person['langs'].append('rust')  # Usamos el metodo .append para agregar un valor al final de la lista
+print(person)
+
+del person['last_name']  # eliminamos el atributo
+person.pop('age')
+print("-----------------------------------------------------------------")
+
+print('name')
+print(person)
+
+
