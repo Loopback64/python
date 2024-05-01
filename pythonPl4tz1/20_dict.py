54cdd387965bb46d640f8810c041b1416e8ab26b
loopback64 <luishever@gmail.com>
2024-05-01 00:30:46 -0500

    Crea leer modificar diccionarios.

--- pythonPl4tz1/20_dict.py
+++ pythonPl4tz1/20_dict.py
@@ -3,12 +3,17 @@ print("                                                                  ")
 print("Mostramos los valores cargados en el diccionario llamado person")
 print("                                                                  ")
 print("****************************************************************************************")
+
+
 person = {
-    'name': 'nico',
-    'last_name': 'molina',
-    'langs': ['python', 'javascript'],
-    'age': 99
+    'name'            : 'nico',
+    'last_name'       : 'molina',
+    'langs'           : ['python', 'javascript'],
+    'age'             : 99
 }
+
+
+
 print("                                                                  ")
 print("---------------------------------------------------------------------------------------")
 print(person)
@@ -22,19 +27,35 @@ print("y mostramos el resultado")
 print("                                                                  ")
 print("***************************************************************************************")
 print("                                                                  ")
-person['name'] = 'santi'  # cambia el valor del atributo name , que en este caso es nico cambiado a santi
-person['age'] -= 50 # al valor actual del atributo le restamos 50
-person['langs'].append('rust')  # Usamos el metodo .append para agregar un valor al final de la lista
+person['name'] = 'santi'   # cambia el valor del atributo name , que en este caso es nico cambiado a santi
+person['age'] -= 50        # al valor actual del atributo le restamos 50
+person['langs'].append('rust')    # Usamos el metodo .append para agregar un valor al final de la lista del atributo.
 
 print(person)
 
-del person['last_name']  # eliminamos el atributo
-person.pop('age')
+del person['last_name']    # eliminamos el atributo
+person.pop('age')          # pop es un metodo que elimina un elemento del diccionario , simepre hay darle un argumento
 print("---------------------------------------------------------------------------------------")
+print("                                                                  ")
+print("                                                                  ")
+print("                                                                  ")
 
-print('name')
+
 print(person)
-print('name2')
+print("                                                                  ")
+
+print('items')
+print("                                                                  ")
+
+print(person.items())       # imprime todos los items del diccionario 
+print("                                                                  ")
+print("                                                                  ")
+print("                                                                  ")
+print("                                                                  ")
+print("                                                                  ")
+print("                                                                  ")
+
+
 print('name3')
 
 
