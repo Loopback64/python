b4726225d895c63526c33b85777c71678241e00e
loopback64 <luishever@gmail.com>
2024-07-16 00:02:42 -0500

    Crea leer modificar diccionarios.

--- pythonPl4tz1/20_dict.py
+++ pythonPl4tz1/20_dict.py
@@ -57,7 +57,7 @@ print("                                                                  ")
 print("                                                                  ")
 print("                                                                  ")
 print("                                                                  ")
-
+print("    ")
 
 print('name4')
 
