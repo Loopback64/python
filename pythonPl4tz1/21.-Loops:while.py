864e37d630a77838f5690f4ef94f68e29cce8c53
loopback64 <luishever@gmail.com>
2024-08-04 01:47:12 -0500

    Ahora usamos un bucle y usamos una condicional. Aumentamos break Aumentamos continue

--- pythonPl4tz1/21.-Loops:while.py
+++ pythonPl4tz1/21.-Loops:while.py
@@ -11,12 +11,24 @@ while counter < 10:
     counter += 1
     print(counter)
 '''
+
+'''
 counter = 0
 while counter < 20:
     counter += 1
     if counter == 15:
         break
     print(counter)
+'''
+
+counter = 0
+while counter < 20:
+    counter += 1
+    if counter < 15:
+        continue
+    print(counter)
+
+
 
 
 
