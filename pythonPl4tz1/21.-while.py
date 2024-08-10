0aea07475a9e85b4b623cdd5b07470e37a954f34
loopback64 <luishever@gmail.com>
2024-08-09 23:44:18 -0500

    Iniciamos usando loop

---
+++ pythonPl4tz1/21.-while.py
@@ -0,0 +1,5 @@
+while True:
+    print('Se ejecuto')
+
+
+
--- pythonPl4tz1/22.-for.py
+++ pythonPl4tz1/22.-for.py
@@ -1,2 +1,28 @@
-for element in range(20):
+
+
+'''
+for element in range(1, 21):
     print(element)
+
+'''
+
+my_list = [23, 45, 67, 89, 43]
+for element in my_list:
+    print(element)
+
+
+my_tuple = ('nico', 'juli', 'santi')
+for element in my_tuple:
+    print(element)
+
+'''
+product = {
+    'name'  = 'Camisa',
+    'price' = 100,
+    'stock' = 89
+}
+
+for element in product:
+    print(element)
+
+'''
